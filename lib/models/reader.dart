5465d15e342e75b86b6a47f09df6b0f25a033727
diegozuro <diegozuro99@gmail.com>
2024-01-12 10:27:21 -0300

    stripe_terminal.podspec

--- lib/models/reader.dart
+++ lib/models/reader.dart
@@ -26,9 +26,7 @@ class StripeReader {
     return StripeReader(
       locationStatus: LocationStatus.values[json["locationStatus"]],
       batteryStatus: BatteryStatus.values[json["batteryStatus"]],
-      deviceType: json["deviceType"] == 10
-          ? DeviceType.mobilePhoneReader
-          : DeviceType.values[json["deviceType"]],
+      deviceType: json["deviceType"] == 10 ? DeviceType.mobilePhoneReader: DeviceType.values[json["deviceType"]],
       originalJSON: Map.from(json["originalJSON"] ?? {}),
       simulated: json["simulated"],
       label: json["label"],
